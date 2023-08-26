50172fcd74c75762bc04687863616244539418c1
Valentinabm01234 <barrosmarchesiv@gmail.com>
2023-08-26 16:45:44 -0300

    finishFront

--- client/src/index.js
+++ client/src/index.js
@@ -1,17 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Provider } from 'react-redux';
+import store from './store/index';
+import axios from 'axios';
+// import dotenv from 'dotenv';
+// dotenv.config()
+
+// axios.defaults.baseURL = REACT_APP_API || "http://localhost:3001"
+axios.defaults.baseURL = "https://video-games-pi-henry-api.vercel.app"
+
 
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store={store}>
     <App />
+    </Provider> 
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
